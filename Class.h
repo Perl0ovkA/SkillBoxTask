d441a8dbbfacf5cc03a4e2579d27ff36c9ade1d0
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-06-25 13:30:46 +0300

    Добавьте файлы проекта.

---
+++ Class.h
@@ -0,0 +1,5 @@
+
+int sum(int a,int b)
+{
+	return pow (a + b,2);
+}
---
+++ SkillBoxTask.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Class.h"
+
+int main()
+{
+    int result = sum(3, 3);
+    std::cout << result << "\n";
+}
